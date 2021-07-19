ca6b90b180ab978a9bcd7127d4dfe5d5fc6773c3
secondar <mail@bugquit.com>
2021-07-19 22:08:11 +0800

    -

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,16 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <ul>
+        <li class="current">Home</li>
+        <li>Bolg</li>
+        <li>About</li>
+      </ul>
     </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+<style scoped>
+@import "~@/assets/css/style.css";
 </style>
+
---
+++ src/views/home/Home.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="audio-main">
+    <AudioPalyer/>
+    <div class="aplayer-component"></div>
+  </div>
+</template>
+<script>
+import AudioPalyer from "@/assets/component/AudioPalyer/AudioPalyer";
+export default {
+  data() {
+    return {
+      audio: new Audio(),
+      duration: 100,
+      currentTime: 0,
+      isGetDuration: false,
+    };
+  },
+  components: {
+    AudioPalyer,
+  },
+  methods: {
+    goToPage(url) {
+      this.$router.push(url);
+    },
+    palyAudio() {
+      this.audio.src =
+        "http://m7.music.126.net/20210719160536/da858b98076fd78e703764b669bacc97/ymusic/555f/560b/025d/b573182bc7aa7b5f57fd577ef3afa38b.mp3";
+      let playPromise;
+      playPromise = this.audio.play();
+      if (playPromise) {
+        setInterval(() => {
+          if (!this.isGetDuration) {
+            this.duration = this.audio.duration;
+            this.isGetDuration = true;
+          }
+          this.currentTime = this.audio.currentTime;
+          console.log();
+        }, 1000);
+      }
+    },
+  },
+};
+</script>
+<style scoped>
+@import "~@/assets/css/style.css";
+</style>
